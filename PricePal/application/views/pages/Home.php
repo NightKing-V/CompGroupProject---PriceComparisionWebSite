322794a6c484788378d7560d26714e6c16017b34
Valenteno Lenora <86039155+NightKing-V@users.noreply.github.com>
2024-03-16 15:04:17 +0530

    UPDATE - UI

--- PricePal/application/views/pages/Home.php
+++ PricePal/application/views/pages/Home.php
@@ -1,6 +1,21 @@
-<div class="section" id="main-left">
-    <?=$sidebar?>
-</div>
-<div class = "section" id="main-body">
 
+<div class = "section" id="main-body">
+    <div class="card" style="width: 18rem;">
+    <img src="https://singerwebcdn.azureedge.net/resources/products/thumbnails/HON-X7B-8-256-SIL-01.webp" class="card-img-top" alt="...">
+    <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+    </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
 </div>
--- PricePal/assets/JS/ActiveNav.js
+++ PricePal/assets/JS/ActiveNav.js
@@ -1,7 +1,7 @@
 const currentLocation = location.href;
 const menuItem = document.querySelectorAll('nav div ul li a');
 const menuLength = menuItem.length;
-for (i = 0; i<menuLength; i++) {
+for (var i = 0; i<menuLength; i++) {
     if (menuItem[i].href === currentLocation) {
         menuItem[i].parentNode.classList.add('active');
         console.log(link);
