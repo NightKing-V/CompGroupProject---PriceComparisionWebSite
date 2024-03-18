4d6244d5336efc8dca3d33dc5254bec33a12a12d
Valenteno Lenora <86039155+NightKing-V@users.noreply.github.com>
2024-03-18 11:51:51 +0530

    update-itemui

--- PricePal/application/views/pages/Home.php
+++ PricePal/application/views/pages/Home.php
@@ -1,21 +1,18 @@
-
-<div class = "section" id="main-body">
-    <div class="card" style="width: 18rem;">
-    <img src="https://singerwebcdn.azureedge.net/resources/products/thumbnails/HON-X7B-8-256-SIL-01.webp" class="card-img-top" alt="...">
-    <div class="card-body">
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<div class="container">
+    <div id="itemgrid" class="row row-cols-4">
+        <?php for ($i = 0; $i < 5; $i++) 
+        {
+        echo '<div class="col items">
+            <div class="card">
+                <img src="https://singerwebcdn.azureedge.net/resources/products/thumbnails/HON-X7B-8-256-SIL-01.webp"
+                    class="card-img-top cardimg" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Price</p>
+                </div>
+            </div>
+        </div>';
+        };
+        ?>
     </div>
-    </div>
-</div>
-
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
 </div>
