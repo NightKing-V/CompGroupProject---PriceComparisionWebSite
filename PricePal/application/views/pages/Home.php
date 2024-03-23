4ababe19511f4211d22c4d002118f0967272bcd0
cupokoffee <118153324+cupokoffee@users.noreply.github.com>
2024-03-23 19:07:45 +0530

    Home.php

--- PricePal/application/views/pages/Home.php
+++ PricePal/application/views/pages/Home.php
@@ -1,6 +1,6 @@
-<h5><a href="<?php echo base_url("index.php/NewArrivals")?>">New Arrivals</a></h5>
 <div class="container">
-    <div id="itemgrid" class="row row-cols-xl-4 row-cols-lg-3 row-cols-md-3 row-cols-sm-2 row-cols-2">
+<h5><a href="<?php echo base_url("index.php/NewArrivals") ?>">New Arrivals</a></h5>
+    <div id="itemgrid" style="margin-top: 20px;" class="row row-cols-xl-4 row-cols-lg-3 row-cols-md-3 row-cols-sm-2 row-cols-2">
         <?php
         foreach ($newarrivals as $document) {
             echo '<div class="col-md-3 card-item">
@@ -17,20 +17,22 @@
                 </div>
                 <div class="card-text text-danger price">
                     <p>';
+            echo '<span style="color:green">';
             echo $document->new_price;
-            echo ' LKR</br><s>';
+            echo ' LKR</br><s style="color:red">';
+            echo '<span style="color:red">';
             echo $document->old_price;
             echo ' LKR</s></p>
             </div>
             <div class="card-text">
-            <p>';
+            <p style="font-size:10px;">';
             $milliseconds = $document->created_at->toDateTime();
             echo $milliseconds->format('Y-m-d');
-            echo'</p>
+            echo '</p >
             </div>
             <div class="row">
             <div class="col" id="item-btn-left"><a href="';
-        echo $document->product_url;
+            echo $document->product_url;
             echo '" class="card-button bg-dark" id="item-btn-left"><span class="material-symbols-outlined">
                 visibility
                 </span></a></div>
@@ -40,8 +42,7 @@
             </div>
             </div>
         </div>';
-        }
-        ;
+        };
         ?>
     </div>
 </div>
