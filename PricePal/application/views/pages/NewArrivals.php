23926a379136bf21dd774c7b9459f984d7ca489e
cupokoffee <118153324+cupokoffee@users.noreply.github.com>
2024-03-23 19:06:51 +0530

    NewArrivals.php

--- PricePal/application/views/pages/NewArrivals.php
+++ PricePal/application/views/pages/NewArrivals.php
@@ -1,5 +1,5 @@
-<h5>New Arrivals</h5>
 <div class="container">
+    <h5>New Arrivals</h5>
     <div id="itemgrid" class="row row-cols-xl-4 row-cols-lg-3 row-cols-md-3 row-cols-sm-2 row-cols-2">
         <?php
         foreach ($newarrivals as $document) {
@@ -17,13 +17,13 @@
                 </div>
                 <div class="card-text text-danger price">
                     <p>';
-            echo $document->old_price;
-            // echo $document->original_price;
-            echo '</br><s>';
-            echo $document->new_price;
-            // echo $document->selling_price;
-            echo '</s></p>
-                </div>
+                    echo '<span style="color:green">';
+                    echo $document->new_price;
+                    echo ' LKR</br><s style="color:red">';
+                    echo '<span style="color:red">';
+                    echo $document->old_price;
+                    echo ' LKR</s></p>
+                    </div>
                 <div class="row">
                 <div class="col" id="item-btn-left"><a href="';
             echo $document->product_url;
