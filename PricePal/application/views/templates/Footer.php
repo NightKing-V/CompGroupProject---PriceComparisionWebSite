b37e538695ea16e980e0263df595fec24a76cc11
cupokoffee <118153324+cupokoffee@users.noreply.github.com>
2024-03-24 13:20:29 +0530

    Footer.php

--- PricePal/application/views/templates/Footer.php
+++ PricePal/application/views/templates/Footer.php
@@ -1,10 +1,31 @@
 <div class="footer">
     <div class="container">
         <div class="row">
-            <div class="col"><a href="<?php echo base_url("index.php/Aboutus")?>">About us</a></div>
-            <div class="col">col</div>
-            <div class="col">col</div>
-            <div class="col">col</div>
+            <div class="col" style="margin-top:20px;display:inline-block;width:45%;">
+                <h4>About</h4>
+                <ul>
+                    <li><a href="<?php echo base_url("index.php/Aboutus")?>">About Us</a></li>
+                    <li><a href="<?php echo base_url("index.php/Aboutus/Contact")?>">Contact</a></li>
+                    <li><a href="<?php echo base_url("index.php/Aboutus/P&P")?>">Privacy & Policy</a></li>
+                </ul>
+            </div>
+            <div class="col" style="margin-top:20px;display:inline-block;width:45%;">
+                <h4>Join Us</h4>
+                <ul>
+                    <li><a href="<?php echo base_url("index.php/Aboutus")?>">Log in</a></li>
+                    <li><a href="<?php echo base_url("index.php/Aboutus/Contact")?>">Sign up</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <div class="row" style="margin-top:20epx;">
+                    <div class="col-1" style="padding-left: 0.5rem;"><img src="https://www.singerslfiles.com/images/singer-logos/singer_logo_white_whiteout_square_bk_transparent.png" style="width:4cm; height:2cm;"></div>
+                    <div class="col-1" style="padding-left: 10rem;"><img src="https://cdn.buyabans.com/media//wysiwyg/BuyAbans_Logo.png" style="width:4cm; height:1.5cm;"></div>
+                    <div class="w-100"></div>
+                    <div class="col-1" style="padding-left: 0.5rem;"><img src="https://www.singhagiri.lk/cdn/shop/files/Sinhagiri-logo_400_120_195x.png?v=1620823118" style="width:4cm; height:1.75cm;"></div>
+                    <div class="col-1" style="padding-left: 10rem;"><img src="https://damro.lk/wp-content/uploads/2019/12/damrologo.png" style="width:4cm; height:2cm;"></div>
+                    
+                </div>
+            </div>
         </div>
     </div>
     </body>
